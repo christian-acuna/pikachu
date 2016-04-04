33f8ab53c83b8a5c7636a3e34963b26d99ca0ebe
Christian Acuna <cacuna0828@gmail.com>
2016-04-04 11:02:17 +0800

    Create custom view class Pokecell for UICollectionView

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Christian on 4/4/16.
+//  Copyright © 2016 Crossroads. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+  
+  @IBOutlet weak var thumbImg: UIImageView!
+  @IBOutlet weak var nameLbl: UILabel!
+  
+  var pokemon: Pokemon! //Pokemon class created in model
+  
+  func configureCell(pokemon: Pokemon) { //pass in a Pokemon object to configure the cell
+    self.pokemon = pokemon
+    
+    nameLbl.text = self.pokemon.name.capitalizedString //capitalize the first word in a sentence
+    thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    
+  }
+    
+}
