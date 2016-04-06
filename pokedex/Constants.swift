565e4e12c8c363da3e85170661ab6912a8a0a6d9
Christian Acuna <cacuna0828@gmail.com>
2016-04-06 14:03:38 +0800

    downloaded and parsed data

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Christian on 4/5/16.
+//  Copyright © 2016 Crossroads. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> () //creating a clouse (a block of code that is going to be called whenever we want it to) empty clousure
